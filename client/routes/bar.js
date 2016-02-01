c139ff87d75e0a28fbb01d4111e7dd95bc9803e7
杨佳军 <yangjj@chezhil.com>
2016-02-01 21:13:15 +0800

    no message

--- client/routes/bar.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import Vue from '../lib/vue.min'
-
-let Bar = Vue.extend({
-    replace : false,
-    template: '<p>This is bar!</p>',
-})
-
-export default Bar
--- client/routes/foo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import Vue from '../lib/vue.min'
-
-let Foo = Vue.extend({
-    replace : false,
-    template: '<p>This is foo!</p>'
-})
-
-export default Foo
