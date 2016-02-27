0612c921786826004157ca8d82634e8a50f07bb2
杨佳军 <yangjj@chezhil.com>
2016-02-27 11:01:35 +0800

    增加env文件

---
+++ env.js
@@ -0,0 +1,16 @@
+var env = {
+    NODE_ENV : 'development',
+    SELF_HOST : '127.0.0.1:3000',
+    APIStore : {
+        api_key   : 'a369f43a6392605426433831e10765ec',
+        news_host : 'http://apis.baidu.com',
+    },
+    PATH_COOKBOOK : __dirname+'/client/com.chezhil.cookbook',
+    PATH_BUILD    : __dirname+'/__build__',
+
+    API_BASE_HOST : 'http://',
+    API_WX_HOST   : 'http://'
+
+}
+
+module.exports.config = env;
