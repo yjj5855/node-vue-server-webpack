4dbb5a1f6d8b8fd8362f254a790ff4300bd58126
杨佳军 <651380735@qq.com>
2016-05-20 13:06:03 +0800

    打包

--- env.es6
+++ env.es6
@@ -1,6 +1,6 @@
 'use strict';
 const env = {
-    //NODE_ENV : 'test',
+    NODE_ENV : 'test',
     PORT : '5669',
 
     host_panda : 'http://www.panda.tv'
--- env.js
+++ env.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var env = {
-    //NODE_ENV : 'test',
+    NODE_ENV: 'test',
     PORT: '5669',
 
     host_panda: 'http://www.panda.tv'
